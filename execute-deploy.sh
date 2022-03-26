918ec1eb198c79b872f383134f6939aef6572002
vgihan <jum654@naver.com>
2022-03-27 01:47:03 +0900

    chore: update deploy

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd /home/vgihan/social-soseol-front
-sudo ln -s /usr/bin/nodejs /usr/local/bin/node
-/root/.nvm/versions/node/v14.17.3/bin/yarn
-/root/.nvm/versions/node/v14.17.3/bin/pm2 start yarn --name Social-Soseol -- start
+yarn
+pm2 start yarn --name Social-Soseol -- start
