319ddd1112fb07722028794726d87910bc40ec44
vgihan <jum654@naver.com>
2022-04-03 17:57:12 +0900

    fix: main.ts -> main.js
    - babel 설정 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  babel: async (options) => {
+    return {
+      ...options,
+      presets: ['@babel/preset-react', '@babel/preset-typescript'],
+    };
+  },
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
