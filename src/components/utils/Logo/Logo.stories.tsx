cee9ceb195b8db34e3292784e68a1f0cc9ca52a9
vgihan <jum654@naver.com>
2022-04-03 17:15:02 +0900

    feat: add Logo Util Component

---
+++ src/components/utils/Logo/Logo.stories.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+
+import Logo from './Logo';
+
+export default { title: 'Templates/Main', component: Logo };
+
+export const Default = () => <Logo />;
---
+++ src/components/utils/Logo/Logo.tsx
@@ -0,0 +1,5 @@
+const Logo = () => {
+  return <img src="img/logo.png" alt="logo" />;
+};
+
+export default Logo;
