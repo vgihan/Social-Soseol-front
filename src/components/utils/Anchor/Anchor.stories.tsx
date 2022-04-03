05994a7bc7e62a198d0bb3303404892250f7d58e
vgihan <jum654@naver.com>
2022-04-03 17:51:56 +0900

    feat: add anchor util component

---
+++ src/components/utils/Anchor/Anchor.stories.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+
+import Anchor from './Anchor';
+
+export default { title: 'Templates/Main', component: Anchor };
+
+export const Default = () => <Anchor>Test</Anchor>;
---
+++ src/components/utils/Anchor/Anchor.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const Anchor = styled.a`
+  padding: 10px;
+  color: black;
+  cursor: pointer;
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+export default Anchor;
