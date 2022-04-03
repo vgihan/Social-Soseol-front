126941efa65a097a4a88ee4e32e5578b1ccb77af
vgihan <jum654@naver.com>
2022-04-03 17:30:13 +0900

    feat: add MainTemplate

---
+++ src/components/templates/MainTemplate/MainTemplate.stories.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+
+import MainTemplate from './MainTemplate';
+
+export default { title: 'Templates/Main', component: MainTemplate };
+
+export const Default = () => <MainTemplate />;
---
+++ src/components/templates/MainTemplate/MainTemplate.tsx
@@ -0,0 +1,15 @@
+import React, { PropsWithChildren } from 'react';
+
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 150px 50px 100px 50px 250px 250px 250px 250px;
+  height: 100%;
+`;
+
+const MainTemplate: React.FC<PropsWithChildren<{}>> = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+export default MainTemplate;
