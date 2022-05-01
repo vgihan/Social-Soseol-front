833f081f08fb8e1e10d7453e0e5a0b4128350d9c
vgihan <jum654@naver.com>
2022-05-01 18:24:05 +0900

    fix: Function Component에서 styled component로 수정

--- src/components/templates/MainTemplate/MainTemplate.tsx
+++ src/components/templates/MainTemplate/MainTemplate.tsx
@@ -1,15 +1,9 @@
-import React, { PropsWithChildren } from 'react';
-
 import styled from '@emotion/styled';
 
-const Container = styled.div`
+const MainTemplate = styled.div`
   display: grid;
   grid-template-rows: 60px 150px 75px 100px 50px 250px 250px 250px 250px;
   height: 100%;
 `;
-
-const MainTemplate: React.FC<PropsWithChildren<{}>> = ({ children }) => {
-  return <Container>{children}</Container>;
-};
 
 export default MainTemplate;
