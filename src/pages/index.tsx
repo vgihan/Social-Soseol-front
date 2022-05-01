e2cda51fdb4b06a41055b52259a07f52c8adc731
vgihan <jum654@naver.com>
2022-05-01 18:52:59 +0900

    feat: 메인 페이지에 LoginModal 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 
 import InputTextArea from '../components/organisms/Main/InputTextArea';
+import LoginModal from '../components/organisms/Main/LoginModal';
 import LogoArea from '../components/organisms/Main/LogoArea';
 import NavigationArea from '../components/organisms/Main/NavigationArea';
 import TopArea from '../components/organisms/Main/TopArea';
@@ -8,12 +9,15 @@ import MainTemplate from '../components/templates/MainTemplate';
 
 const Home: NextPage = () => {
   return (
-    <MainTemplate>
-      <TopArea />
-      <LogoArea />
-      <NavigationArea />
-      <InputTextArea />
-    </MainTemplate>
+    <>
+      <MainTemplate>
+        <TopArea />
+        <LogoArea />
+        <NavigationArea />
+        <InputTextArea />
+      </MainTemplate>
+      <LoginModal />
+    </>
   );
 };
 
