366d61bdf91e42792d12cec80d182db8bc5551e5
vgihan <jum654@naver.com>
2022-05-02 23:24:31 +0900

    feat: kakaoLoginButton 반응형 개선
    - 단순 width 줄이기로는 이미지가 깨져서, 작은 로그인 버튼 리소스를
    다운로드 받아 css에서 url을 변경시켜줌.

--- src/components/utils/KakaoLoginButton/KakaoLoginButton.tsx
+++ src/components/utils/KakaoLoginButton/KakaoLoginButton.tsx
@@ -1,9 +1,20 @@
 import { AnchorHTMLAttributes, DetailedHTMLProps } from 'react';
 
+import styled from '@emotion/styled';
+
+import theme from '../../../styles/theme';
+
+const KakaoButtonImage = styled.img`
+  content: url('/img/kakao_login_long.png');
+  @media (max-width: ${theme.breakpoints.md}) {
+    content: url('/img/kakao_login_short.png');
+  }
+`;
+
 const KakaoLoginButton = (props: DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) => {
   return (
-    <a {...props}>
-      <img src="/img/kakao_login.png" alt="kakao" />
+    <a {...props} style={{ textAlign: 'center' }}>
+      <KakaoButtonImage alt="kakao" />
     </a>
   );
 };
