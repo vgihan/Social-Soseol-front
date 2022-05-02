84dfe3576ba1f2d569ccab04878c59a26ffdc2b4
vgihan <jum654@naver.com>
2022-05-02 22:32:16 +0900

    feat: 메인 페이지 모달 제어 action 및 초기화 action 추가
    - closeLoginModal action 발생 시 Modal off
    - openLoginModal action 발생 시 Modal on
    - init action 발생 시 mainReducer state 초기화

---
+++ src/store/actions/main/closeLoginModal.ts
@@ -0,0 +1,2 @@
+export const CLOSE_LOGIN_MODAL = 'CLOSE_LOGIN_MODAL';
+export const closeLoginModalAction = () => ({ type: CLOSE_LOGIN_MODAL });
---
+++ src/store/actions/main/openLoginModal.ts
@@ -0,0 +1,2 @@
+export const OPEN_LOGIN_MODAL = 'OPEN_LOGIN_MODAL';
+export const openLoginModalAction = () => ({ type: OPEN_LOGIN_MODAL });
