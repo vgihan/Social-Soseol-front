b5e1d107d8bb30645fca31f5c67d88258059489b
vgihan <jum654@naver.com>
2022-05-02 22:28:11 +0900

    feat: dropdown의 로그인 메뉴 클릭 핸들러 수정
    - 로그인 메뉴 클릭 시 redux 전역 상태 isOpenModal을 true로 변경

--- src/components/organisms/Main/TopArea/TopArea.tsx
+++ src/components/organisms/Main/TopArea/TopArea.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
-import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
 
+import { openLoginModalAction } from '../../../../store/actions/main/openLoginModal';
 import Dropdown from '../../../utils/Dropdown';
 import FlexCenter from '../../../utils/FlexCenter';
 import Profile from '../../../utils/Profile';
@@ -14,18 +15,13 @@ const ElementBox = styled.div`
 `;
 
 const TopArea = () => {
-  const router = useRouter();
+  const dispatch = useDispatch();
 
   return (
     <Container>
       <ElementBox />
       <ElementBox>
-        <Dropdown
-          menus={[
-            { name: '로그인', handler: () => router.push('/login') },
-            { name: '로그아웃', handler: () => router.push('/') },
-          ]}
-        >
+        <Dropdown menus={[{ name: '로그인', handler: () => dispatch(openLoginModalAction()) }]}>
           <Profile />
           <img src="/img/dropdown.svg" alt="dropdown" height="100%" />
         </Dropdown>
