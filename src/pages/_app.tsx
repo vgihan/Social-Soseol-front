205d6b30d6ffb29fa2a68eafe431337673a4110b
vgihan <jum654@naver.com>
2022-05-02 22:31:26 +0900

    feat: add store provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+
 import '../../public/css/reset.css';
+import store from '../store';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>소셜 소설</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 };
 
