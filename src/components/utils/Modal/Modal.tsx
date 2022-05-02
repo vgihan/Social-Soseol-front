f1da2c1d657f0afedcf7e2df1ea68e1c1818e54f
vgihan <jum654@naver.com>
2022-05-02 23:25:25 +0900

    feat: Modal size 반응형 개선

--- src/components/utils/Modal/Modal.tsx
+++ src/components/utils/Modal/Modal.tsx
@@ -2,12 +2,14 @@ import { MouseEventHandler, PropsWithChildren } from 'react';
 
 import styled from '@emotion/styled';
 
+import theme from '../../../styles/theme';
 import FlexCenter from '../FlexCenter';
 import Portal from '../Portal';
 
 const Background = styled(FlexCenter)`
   background-color: rgba(0, 0, 0, 0.4);
   position: fixed;
+  z-index: 100;
   top: 0;
   bottom: 0;
   width: 100%;
@@ -15,9 +17,23 @@ const Background = styled(FlexCenter)`
 `;
 const Frame = styled.div`
   box-shadow: 0 1px 6px 0 rgba(32, 33, 36, 0.28);
+  background-color: white;
+  padding: 40px 0;
+  box-sizing: border-box;
   width: 500px;
   height: 650px;
-  background-color: white;
+  @media (max-width: ${theme.breakpoints.lg}) {
+    width: 400px;
+    height: 600px;
+  }
+  @media (max-width: ${theme.breakpoints.md}) {
+    width: 400px;
+    height: 500px;
+  }
+  @media (max-width: ${theme.breakpoints.sm}) {
+    width: 250px;
+    height: 400px;
+  }
 `;
 
 type Props = {
